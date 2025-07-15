36c150d3e045b3b8eb116f007e413a98124e4b03
hmj2422021 <hmj2422021@stu.o-hara.ac.jp>
2025-07-15 23:07:38 +0900

    カードの回転アニメーションを修正

--- Assets/Cards/CardController.cs
+++ Assets/Cards/CardController.cs
@@ -10,33 +10,37 @@ public class CardController : MonoBehaviour
 
     private Image m_image;
     private RectTransform m_rectTransform;
-    private bool m_isFront;
 
     private void Awake()
     {
         m_image = GetComponent<Image>();
         m_rectTransform = GetComponent<RectTransform>();
-        m_isFront = false;
+
+        //裏面から開始する
+        m_image.sprite = m_cardBack;
     }
 
-    private void OnEnable()
+    private void OnDisable()
     {
+        //裏面から開始する
         m_image.sprite = m_cardBack;
+
+        //途中で非表示にした場合も角度を初期位置に戻す
+        m_rectTransform.rotation = Quaternion.Euler(Vector3.zero);
     }
 
     public IEnumerator Flip()
     {
         float tick = 0;
-        Vector3 startRotate = new Vector3(0, 0, 0);
         Vector3 endRotate = new Vector3(0, 90, 0);
         Vector3 rotate;
 
-        //最初から中間地点までひっくり返す
+        //最初から中間まで回転させる
         while (tick < 1.0f)
         {
             tick += Time.deltaTime * m_flipSpeed;
 
-            rotate = Vector3.Lerp(startRotate, endRotate, tick);
+            rotate = Vector3.Lerp(Vector3.zero, endRotate, tick);
 
             m_rectTransform.rotation = Quaternion.Euler(rotate);
 
@@ -48,12 +52,12 @@ public class CardController : MonoBehaviour
 
         tick = 0f;
 
-        //中間から最後までひっくり返す
+        //中間から最後まで最初から中間地点まで回転させる
         while (tick < 1.0f)
         {
             tick += Time.deltaTime * m_flipSpeed;
 
-            rotate = Vector3.Lerp(endRotate, startRotate, tick);
+            rotate = Vector3.Lerp(endRotate, Vector3.zero, tick);
 
             m_rectTransform.rotation = Quaternion.Euler(rotate);
 
